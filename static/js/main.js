c23f6a3970ef7f6011ba5572787157cfa6896ab0
PietroAlkmin <pietro.alkmin@sou.inteli.edu.br>
2026-04-08 10:46:20 -0300

    feat: paginas de gestao e historico front-end (#4)

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+function deletarLeitura(id) {
+    if (confirm(`Tem certeza que deseja deletar a leitura #${id}?`)) {
+        fetch(`/leituras/${id}/deletar`, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'deletado') {
+                // Remover a linha da tabela
+                const row = document.getElementById(`row-${id}`);
+                if (row) row.remove();
+                alert('Leitura deletada com sucesso!');
+            } else {
+                alert('Erro ao deletar leitura.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+            alert('Ocorreu um erro de rede ao tentar deletar.');
+        });
+    }
+}
